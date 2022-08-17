72ed0ba52adc8b64b351d87e76dce0f6d0a59d08
Hernando Sanabria <nanaditosansan@gmail.com>
2022-08-16 22:22:25 -0400

    he agregado el programa de adicion con clases

---
+++ COVID19/CODIGO/adicion.py
@@ -0,0 +1,5 @@
+import Covid
+covid2=Covid.Covid('NombreProvisional.png','C:/Users/LicHernandoSanabria/MODULOII/PROYECTOFINAL/COVID19')
+print(covid2.nombre)
+covid2.adicionar('95878.jpg','95878i.jpg','http://todo2.com','IMAGENESADICION/imagenes','IMAGENESADICION/mascaras')
+
